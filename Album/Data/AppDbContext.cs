8c1864f22c5aa9bd24316a5ace647e095d5dde14
derek089 <nguyen.hai.dang@icloud.com>
2023-01-17 10:14:05 +0700

    add scaffold

---
+++ Album/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Album.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Album.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+    {
+    }
+}
---
+++ Album/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Album.Models;
+
+public class AppUser : IdentityUser
+{
+    
+}
